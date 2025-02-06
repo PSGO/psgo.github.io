78494e61566c5b23b87a3f5f458b3e99a1170d59
chuanyuekongjian <psgo.tk@outlook.com>
2025-02-06 23:19:47 +0800

    启用sw.js

---
+++ switch-sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'switch-20250206';//更新缓存只需要改变名称字符
+const ASSETS = [
+    '/bg.jpg'
+];
+
+// 安装 Service Worker 并缓存资源
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// 拦截请求，返回缓存内容
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// 更新缓存（如果需要）
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.map(key => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
