f6913d1fc81483ca400ba8dd8c3821e8cc53e774
chuanyuekongjian <psgo.tk@outlook.com>
2025-03-07 12:31:13 +0800

    .

---
+++ 999/payload_map.js
@@ -0,0 +1,101 @@
+const payload_map =
+    [
+        {
+            displayTitle: 'etaHEN v2.0b-pre2（解锁运行PS5/PS4）',
+            description: '多合一HEN，支持金手指，支持 3.00 - 4.51',
+            fileName: 'etaHEN.bin',
+            author: '-',
+            source:'-',
+            version: '-'
+        },
+		{
+            displayTitle: 'etaHEN v1.8b（解锁运行PS5/PS4）',
+            description: '支持itemzFlow，支持 3.00 - 4.51 所有版本',
+            fileName: 'etaHEN0.bin',
+            author: '-',
+            source:'-',
+            version: '-'
+        },
+		{
+            displayTitle: 'Kstuff v1.4（推荐 / 仅解锁运行PS4）',
+            description: '不要重复点击！和etaHEN只能二选一！支持 3.00 - 4.51 所有版本',
+            fileName: 'ps5-kstuff.bin',
+            author: '-',
+            source:'-',
+            version: '-'
+        },
+		{
+            displayTitle: 'FTP远程文件管理',
+            description: '端口号1337，后台持续运行，配合FileZilla电脑客户端',
+            fileName: 'ftps5-p.elf',
+            author: '-',
+            source:'-',
+            version: '-'
+        },
+		{
+            displayTitle: 'elf/bin插件loader加载器',
+            description: '端口号9021，后台持续运行，配合NetCat或安卓客户端推送插件',
+            fileName: 'elfldr.elf',
+            author: '-',
+            source:'-',
+            version: '-'
+        },
+		{
+            displayTitle: 'Clear cache 清除浏览器缓存',
+            description: 'PS5无法双清，遇到缓存异常问题使用本功能清除后重新缓存',
+            fileName: 'Browser_appCache_remove.elf',
+            author: '-',
+            source:'-',
+            version: '-'
+        },
+		{
+            displayTitle: 'Block updates 屏蔽系统更新',
+            description: '左上角显示“blocker enable”则为永久生效，建议配合路由器屏蔽',
+            fileName: 'lock_unlock_updates.elf',
+            author: '-',
+            source:'-',
+            version: '-'
+        }, 
+        {
+            displayTitle: 'BackupDB 备份数据（解决图标消失）',
+            description: '索引、存档等数据，点击前插入exfat格式U盘，建议每月备份一次',
+            fileName: 'Backup-db-PS5.elf',
+            author: '-',
+            source:'-',
+            version: '-'
+        },
+		{
+            displayTitle: 'PS5debug v1.0b2（金手指）',
+            description: '配合MultiTrainer II电脑客户端或内存调试工具',
+            fileName: 'ps5debug.elf',
+            author: '-',
+            source:'-',
+            version: '-'
+        },
+		{
+            displayTitle: '解锁部分正版游戏120/60帧率',
+            description: 'libhijacker v1.160 运行后可直接启动相应版本游戏',
+            fileName: 'spawner.elf',
+            author: '-',
+            source:'-',
+            version: '-'
+        },
+		{
+            displayTitle: '查看版本信息',
+            description: '内核、系统、SDK等版本信息',
+            fileName: 'versions.elf',
+            author: '-',
+            source:'-',
+            version: '-'
+        },
+		{
+            displayTitle: '查看系统状态',
+            description: '温度、频率等系统信息',
+            fileName: 'hwinfo-tornblom.elf',
+            author: '-',
+            source:'-',
+            version: '-'
+        }
+		
+
+    ];
